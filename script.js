437d0d466de95e73b24f1f6c4c40e22aef7b8329
monicaalv <moni5966@stud.kea.dk>
2021-10-11 10:25:10 +0200

    js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-WebGLSampler.to(".info1", { duration: 2, x: 300 });
+gsap.registerPlugin(ScrollTrigger);
+
+const sections = document.querySelectorAll("#section1, #section4, #section6");
+
+sections.forEach((section) => {
+  const h2 = section.querySelector("h2");
+  const h3 = section.querySelectorAll("h3");
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: "top 50%",
+        end: "+=200",
+        scrub: true,
+        markers: true,
+      },
+    })
+    .from(h2, {
+      duration: 1,
+      opacity: 0,
+    })
+    .from(h3, {
+      y: 100,
+      duration: 1,
+      opacity: 0,
+      stagger: 1,
+    });
+});
